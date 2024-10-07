5357e223e04b3f7f6fab65748a5f2018043dd78b
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-10-07 13:22:31 +0200

    feat: add shuffle functionality that shuffles all cards when state changes

--- src/components/MemoryCards.jsx
+++ src/components/MemoryCards.jsx
@@ -2,85 +2,53 @@ import { useEffect, useState } from "react"
 
 const API_KEY = 'd8ee147c3c98420d8d2c5b7ef65b5cd6';
 const RAWG_2004_URL = 'https://api.rawg.io/api/games?dates=2004-01-01,2004-12-31&page_size=40&key=';
-const RAWG_2004_URL_2 = 'https://api.rawg.io/api/games?dates=2004-01-01,2004-12-31&page=3&key=';
+const RAWG_2004_URL_2 = 'https://api.rawg.io/api/games?dates=2004-01-01,2004-12-31&page_size=40&page=2&key=';
+const RAWG_2004_URL_3 = 'https://api.rawg.io/api/games?dates=2004-01-01,2004-12-31&page=5&key=';
 
 export default function MemoryCards() {
 	const { data: page1, error: error1 } = useFetchData(`${RAWG_2004_URL}${API_KEY}`);
 	const { data: page2, error: error2 } = useFetchData(`${RAWG_2004_URL_2}${API_KEY}`);
-
-	console.log(page1)
-	console.log(page2)
-
+	const { data: page3, error: error3 } = useFetchData(`${RAWG_2004_URL_3}${API_KEY}`);
+	const [score, setScore] = useState(0);
+	const [bestScore, setBestScore] = useState(0);
+	
 	// state changes on click registering:
 	// 1. a point for the card not previously clicked
 	// 2. reset game if card was previously clicked
 	// state change in both cases would reshufle the cards
 	// so have a shuffling func that will be triggered here
 
-	if (!page1 || !page2) {
+	if (!page1 || !page2 || !page3) {
 		return <div>Loading...</div>
-	}
+	};
 
-	if (error1 || error2) {
+	if (error1 || error2 || error3) {
 		return (
 			<div>Error: {error1}, {error2}</div>
 		)
-	}
-	
+	};
+
+	// Array of games picked from the API to be displayed
+	const games = [
+		page1[0], page1[1], page1[4], 
+		page1[9], page1[10], page1[12], 
+		page1[13], page1[19], page1[24], 
+		page1[29], page1[33], page1[34], 
+		page1[37], page2[3], page2[16], 
+		page3[7]
+	];
+	// Shuffle cards on every rerender
+	shuffleArray(games);
+	// Map through the array and display game cards
 	return (
 		<div className="cards-cont">
-			<GameCard 
-				game={page1[0]}
-			/>
-			<GameCard 
-				game={page1[1]}
-			/>
-			<GameCard 
-				game={page1[9]}
-			/>
-			<GameCard 
-				game={page1[10]}
-			/>
-			<GameCard 
-				game={page1[12]}
-			/>
-			<GameCard 
-				game={page1[18]}
-			/>
-			<GameCard 
-				game={page1[19]}
-			/>
-			<GameCard 
-				game={page1[4]}
-			/>
-			<GameCard 
-				game={page1[13]}
-			/>
-			<GameCard 
-				game={page1[37]}
-			/>
-			<GameCard 
-				game={page2[16]}
-			/>
-			<GameCard 
-				game={page1[29]}
-			/>
-			<GameCard 
-				game={page1[34]}
-			/>
-			<GameCard 
-				game={page1[33]}
-			/>
-			<GameCard 
-				game={page2[3]}
-			/>
-			<GameCard 
-				game={page1[24]}
-			/>
+			{games.map((game) => {
+				return <GameCard key={game.id} game={game} />
+			})}
 		</div>
 	)
-}
-
+};
+// TODO: Refactor into a separate module
 function GameCard({ game }) {
 	return (
 		<div 
@@ -93,7 +61,14 @@ function GameCard({ game }) {
 			</div>
 		</div>
 	)
-}
+};
+// Takes in an array and shuffles items inside (Fisher-Yates Shuffle)
+function shuffleArray(arr) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+};
 
 function useFetchData(url) {
 	const [data, setData] = useState(null);
@@ -134,4 +109,4 @@ function useFetchData(url) {
 	}, [url])
 
 	return { data, error };
-}
+};
